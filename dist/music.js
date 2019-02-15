e34b6f7816cef46119eda03385046698f3e14f5b
博客 <1422493060@qq.com>
2019-02-15 22:12:01 +0800

    Site updated: 2019-02-15 22:12:00

--- dist/music.js
+++ dist/music.js
@@ -1,22 +1,19 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
-    autoplay: false,
-    loop: 'all',
-    volume: 0.7,
-    listFolded: true,
-    listMaxHeight: 60,
+    fixed: true,
+    autoplay: true,   //自动播放
+    listFolded:false, //列表默认折叠
+    theme: '#FADFA3', //主题色
+    preload: 'auto', //预加载，可选值: 'none', 'metadata', 'auto'
+    mutex: true, //互斥，阻止多个播放器同时播放，当前播放器播放时暂停其他播放器
+    lrcType: 2,
     audio: [
-        {
-            name: 'name1',
-            artist: 'artist1',
-            url: 'url1.mp3',
-            cover: 'cover1.jpg',
-        },
-        {
-            name: 'name2',
-            artist: 'artist2',
-            url: 'url2.mp3',
-            cover: 'cover2.jpg',
-        }
+      {
+        name: '前前前世',
+        artist: 'RADWIMPS',
+        url: 'https://cn-east-17-aplayer-35525609.oss.dogecdn.com/yourname.mp3',
+        cover: 'https://cn-east-17-aplayer-35525609.oss.dogecdn.com/yourname.jpg',
+        lrc: 'https://cn-east-17-aplayer-35525609.oss.dogecdn.com/hikarunara.lrc'
+    }
     ]
 });
